d9b3fa0351e5afd84d2281abf043f8f544564bd3
Dario Vigil <dariovigil@gmail.com>
2018-01-31 20:00:14 +0100

    minor changes

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
-const Battle = () => (
-    <div>
-      BATTLE!
-    </div>
-  )
+class Battle extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      playerOneName: '',
+      playerOneImage: '',
+      playerTwoName: null,
+      playerTwoImage: null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username) {
+    this.setState(function() {
+      let newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = `https://github.com/${username}.png?size=200`;
+      return newState;
+    });
+  }
+
+  render() {
+    const playerOneName = this.state.playerOneName;
+    const playerTwoName = this.state.playerTwoName;
+
+    return (
+      <div>
+        <div className='row'>
+          {!playerOneName && <PlayerInput id='playerOne' label='Player One' onSubmit={this.handleSubmit}/>}
+          {!playerTwoName && <PlayerInput id='playerTwo' label='Player Two' onSubmit={this.handleSubmit}/>}
+  
+        </div>
+      </div>
+    )
+  }
+}
 export default Battle;
